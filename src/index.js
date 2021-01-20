0a9f9b9c3d08d4257988fb2bb406b0f94c9a448b
Joaribero <joaribero_la12@hotmail.com>
2021-01-20 20:46:09 -0300

    Add Passport authentication

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const mysqlstore = require('express-mysql-session');
 const {database} = require('./keys');
+const passport = require('passport');
 
 // initializations
 const app = express();
+require('./lib/passport');
 
 // settings
 app.set('port', process.env.PORT || 4000);
@@ -33,11 +35,13 @@ app.use(flash());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 // global variables
 app.use((req,res,next) => {
     app.locals.success = req.flash('success');
+    app.locals.message = req.flash('message');
     next();
 });
 
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,4 +1,32 @@
 const express = require('express');
+const { route } = require('.');
 const router = express.Router();
+const passport = require('passport');
+
+router.get('/signup', (req,res) => {
+    res.render('auth/signup');
+});
+
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+}));
+
+router.get('/signin',(req,res) => {
+    res.render('auth/signin');
+})
+
+router.post('/signin', (req,res,next) => {
+    passport.authenticate('local.signin', {
+        successRedirect: '/profile',
+        failureRedirect: '/signin',
+        failureFlash: true
+    })(req,res,next);
+});
+
+router.get('/profile', (req,res) => {
+    res.send('Profile');
+})
 
 module.exports = router;
